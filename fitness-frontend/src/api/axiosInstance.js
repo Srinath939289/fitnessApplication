74abd2a61b7190e04160f861b35645135e20f8d4
Srinath939289 <153853367+Srinath939289@users.noreply.github.com>
2025-12-08 02:35:05 +0530

    Update axiosInstance.js
    changed existing url with deployed backend url

--- fitness-frontend/src/api/axiosInstance.js
+++ fitness-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://fitnessapplication-n1uh.onrender.com",
+  baseURL: "https://fitnessapplication-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
